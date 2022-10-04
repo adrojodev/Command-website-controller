324cf948f2add70127a9b74a1d65d11f93aa7adb
David Hernandez <rojohernandez75@gmail.com>
2022-10-04 09:46:21 -0500

    Changed to Netlify edge functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify/edge-functions";
 
 import react from "@astrojs/react";
 
