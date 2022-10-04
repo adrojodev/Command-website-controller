8ba96508c5ed707c9251b33cbe50708104447aa3
David Hernandez <rojohernandez75@gmail.com>
2022-10-04 09:21:19 -0500

    Added netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify/functions";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: netlify(),
   integrations: [react()],
   build: {
     rollupOptions: {
